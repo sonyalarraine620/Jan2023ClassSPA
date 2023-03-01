343c1fb7a1a7c1f9814dbd0be2c0cd7e77dd8b83
Sonya Villarruel <sonyalarraine620@outlook.com>
2023-02-28 19:15:43 -0800

    Server update

--- index.js
+++ index.js
@@ -16,11 +16,11 @@ function render(state = store.Home) {
   ${Main(state)}
   ${Footer()}
   `;
-  afterRender();
+  afterRender(state);
   router.updatePageLinks();
 }
 
-function afterRender() {
+function afterRender(state) {
   // add menu toggle to bars icon in nav bar
   document.querySelector(".fa-bars").addEventListener("click", () => {
     document.querySelector("nav > ul").classList.toggle("hidden--mobile");
